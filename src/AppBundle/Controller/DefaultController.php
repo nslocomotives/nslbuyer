7d404ccca8c2493319192e3b8237542715da9d27
Nigel Smith <nslocomotives@hotmail.com>
2017-09-30 18:44:06 +0000

    added theme for login page, tweaks to FOSuser and added notification modules

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -34,8 +35,19 @@ class DefaultController extends Controller
         ));
     }
 	
+	/**
+	* @Route("/admin", name="admin")
+	* @Security("has_role('ROLE_SUPER_ADMIN')")
+	*/
+	public function adminAction()
+	{
+		$data = [];
+		return $this->render('default/admin.html.twig',$data);
+	}
+	
     /**
      * @Route("/finding", name="finding")
+	 * @Security("has_role('ROLE_SUBSCRIBER')")
      */
     public function findAction(Request $request)
     {
@@ -49,5 +61,25 @@ class DefaultController extends Controller
 		
 		// replace this example code with whatever you need
         return $this->render('default/finding.html.twig', $data);
+    }
+	
+	/**
+     * @Route("/send-notification", name="send_notification")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function sendNotification(Request $request)
+    {
+        $manager = $this->get('mgilet.notification');
+        $notif = $manager->generateNotification('Hello world !');
+        $notif->setMessage('This a notification.');
+        $notif->setLink('http://symfony.com/');
+        $manager->addNotification($this->getUser(), $notif);
+
+
+        // or the one-line method :
+        // $manager->createNotification($this->getUser(), 'Notification subject','Some random text','http://google.fr');
+
+        return $this->redirectToRoute('homepage');
     }
 }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,14 +3,17 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Mgilet\NotificationBundle\Model\UserNotificationInterface;
+
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="nslbuyer_user")
  */
-class User extends BaseUser
+class User extends BaseUser implements UserNotificationInterface
 {
 
     /**
@@ -19,10 +22,61 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+	
+	// link to notifications
+    /**
+     * @var Notification
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Notification", mappedBy="user", orphanRemoval=true)
+     */
+    protected $notifications;
 
     public function __construct()
     {
         parent::__construct();
         // your own logic
+		$this->notifications = new ArrayCollection();
+    }
+	
+	// method implementation for UserNotificationInterface
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNotifications()
+    {
+        return $this->notifications;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addNotification($notification)
+    {
+        if (!$this->notifications->contains($notification)) {
+            $this->notifications[] = $notification;
+            $notification->setUser($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeNotification($notification)
+    {
+        if ($this->notifications->contains($notification)) {
+            $this->notifications->removeElement($notification);
+        }
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIdentifier()
+    {
+        $this->getId();
     }
 }
